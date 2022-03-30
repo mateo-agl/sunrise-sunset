f3ae11a1fc697c89cb09d26ee8aed3694d436876
mateo-agl <aguilarmateo.1604@gmail.com>
2022-03-30 15:21:50 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+
+navigator.geolocation.getCurrentPosition(
+  position => {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=2022-03-26`;
+    axios.get(url)
+    .then(res => {
+      const data = res.data.results;
+      let sunrise = data.sunrise;
+      let sunset = data.sunset.split(":");
+      let length = data.day_length;
+      const timeOffset = new Date().getTimezoneOffset() / 60000;
+      const toMiliseconds = (time) => {
+        let miliseconds = 0;
+        const nums = time.split(":"),
+        hours = nums[0],
+        minutes = nums[1],
+        seconds = nums[2].slice(0,2);
+        miliseconds += hours * 3600000;
+        miliseconds += minutes * 60000;
+        miliseconds += seconds * 1000;
+        return miliseconds - timeOffset;
+      }
+      console.log(1 - -1)
+    })
+    .catch(e => console.error(e))
+  }
+)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +38,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
